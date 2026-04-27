38d931461d98f07916fb21e005620ea762483e16
ibarhuttaqi <ibarhuttaqi26@gmail.com>
2026-04-27 13:06:34 +0700

    Initial commit

---
+++ src/main/java/ibarhuttaqisulthon/springrestfullapi/SpringrestfullapiApplication.java
@@ -0,0 +1,13 @@
+package ibarhuttaqisulthon.springrestfullapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestfullapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestfullapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibarhuttaqisulthon/springrestfullapi/SpringrestfullapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ibarhuttaqisulthon.springrestfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestfullapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
